4742529b1b3eb27731db8bf334bd8f2e5651755c
Georgina henderson <georginahenderson@Georginas-MacBook-Pro.local>
2018-03-18 18:39:49 +0000

    To do List practise

--- JS course/Getting Started/script.js
+++ JS course/Getting Started/script.js
@@ -1,1 +1,22 @@
-alert('hello external');
+//document.write('Hello world external');
+//alert('Yes.. this is an alert powered by an external JS File.. :) ');
+
+/*console.log('Hello World');
+console.log(400/10);
+console.error('error');
+console.warn('warn');
+console.log("%cHello world", "color:gold; background:blue");*/
+
+
+
+/*alert("welcome to the end of section one!");
+document.write("Girl.. you made it!");
+console.log('damn.. it worked.. awesome :)');*/
+
+    
+    $( "li" ).click(function() {
+        $( this ).toggleClass( "strikeout" );
+      });
+      
+
+
